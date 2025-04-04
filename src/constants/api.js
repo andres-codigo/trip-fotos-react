190ca233dd91393c6bad01d0633dfe08910abc39
andres-codigo <52874360+andres-codigo@users.noreply.github.com>
2025-04-04 11:44:07 +1100

    [js] API constants
    - api.js constants file added

---
+++ src/constants/api.js
@@ -0,0 +1,14 @@
+export const APIConstants = Object.freeze({
+	// API
+	API_URL: import.meta.env.VITE_API_URL,
+	API_KEY: import.meta.env.VITE_API_KEY,
+	API_AUTH_LOGIN_MODE: 'login',
+	API_AUTH_SIGNUP_MODE: 'signup',
+
+	// DATABASE
+	BASE_URL: import.meta.env.VITE_BACKEND_BASE_URL,
+	GET: 'GET',
+	POST: 'POST',
+	PUT: 'PUT',
+	DELETE: 'DELETE',
+});
