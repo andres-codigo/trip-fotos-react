8a4c085c656319d4986b7d1dbccb0338599fb16a
andres-codigo <52874360+andres-codigo@users.noreply.github.com>
2025-04-09 13:39:38 +1000

    [js] App.js
    - auth store's tryLogin action dispatch added; commented out future react router once didAutoLogout action added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { useNavigate } from 'react-router-dom';
+import { tryLogin } from './store/slices/authSlice';
+
 import './App.module.scss';
 import Header from './components/header/Header';
 
 function App() {
+	const dispatch = useDispatch();
+	// const navigate = useNavigate();
+
+	// Access the `didAutoLogout` state from the Redux store
+	// const didAutoLogout = useSelector((state) => state.auth.didAutoLogout);
+
+	// Dispatch `tryLogin` on component mount
+	useEffect(() => {
+		dispatch(tryLogin());
+	}, [dispatch]);
+
+	// Watch for changes in `didAutoLogout` and redirect to the login page
+	// useEffect(() => {
+	// 	if (didAutoLogout) {
+	// 		navigate('/auth'); // Redirect to the login page
+	// 	}
+	// }, [didAutoLogout, navigate]);
+
 	return <Header />;
 }
 
