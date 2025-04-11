5985dbb8fc8a1b20789015c35024050b7a5a7811
andres-codigo <52874360+andres-codigo@users.noreply.github.com>
2025-04-11 16:56:30 +1000

    [js] Created global constant js

---
+++ src/constants/global.js
@@ -0,0 +1,11 @@
+export const GLOBAL = Object.freeze({
+	// ADMIN
+	ADMIN_ID: import.meta.env.VITE_ADMIN_ID,
+
+	// DIALOG
+	ERROR_DIALOG_TITLE: 'An error occurred',
+	AUTHENTICATING_DIALOG_TITLE: 'Authenticating...',
+
+	// IMAGE UPLOAD
+	LOADING_IMAGE: 'loading',
+});
