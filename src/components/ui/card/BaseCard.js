55580cf1141bb55fb19ec8069920e632c27418ae
andres-codigo <52874360+andres-codigo@users.noreply.github.com>
2025-04-16 15:51:55 +1000

    [js, scss] BaseCard component vue to react added

---
+++ src/components/ui/card/BaseCard.js
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types';
+import baseStyles from './BaseCard.module.scss';
+
+function BaseCard({ children }) {
+	return <div className={baseStyles.card}>{children}</div>;
+}
+
+BaseCard.propTypes = {
+	children: PropTypes.node.isRequired,
+};
+
+export default BaseCard;
