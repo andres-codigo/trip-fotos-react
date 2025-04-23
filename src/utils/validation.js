1db5d7ffcedb7e2ffcb78fca69c3352b7509c324
andres-codigo <52874360+andres-codigo@users.noreply.github.com>
2025-04-23 14:28:12 +1000

    Create validation.js
    - validation utility created to clean UserAuth component and to be reusable across project

---
+++ src/utils/validation.js
@@ -0,0 +1,25 @@
+import { VALIDATION_MESSAGES } from '@/constants/validation-messages';
+
+export const validateEmail = (value) => {
+	const isValid =
+		/^[a-zA-Z0-9]+(?:[._-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:[._-][a-zA-Z0-9]+)*\.[a-zA-Z]{2,6}$/.test(
+			value,
+		);
+
+	return {
+		isValid,
+		message: isValid ? '' : VALIDATION_MESSAGES.EMAIL_INVALID,
+	};
+};
+
+export const validatePassword = (value) => {
+	const difference = 6 - value.length;
+
+	return {
+		isValid: value.length >= 6,
+		message:
+			value.length >= 6
+				? ''
+				: VALIDATION_MESSAGES.PASSWORD_TOO_SHORT(difference),
+	};
+};
