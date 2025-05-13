70aeb6d107b3d92c88beb2acc6f016e64db7da60
andres-codigo <52874360+andres-codigo@users.noreply.github.com>
2025-05-14 09:07:54 +1000

    [js] Update commands.js
    - added viewports and top navigation hamburger menu commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,15 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import { viewports, topNavigationSelectors } from './constants';
+
+// VIEWPORTS
+Cypress.Commands.add('setViewportToMobile', () => {
+	cy.window().then((win) => {
+		cy.viewport(viewports.mobile.width, win.innerHeight);
+	});
+});
+
+// TOP NAVIGATION HAMBURGER MENU
+Cypress.Commands.add('assertHamburgerMenuState', (isActive) => {
+	cy.get(topNavigationSelectors.navHamburgerMenu)
+		.invoke('attr', 'class')
+		.should(isActive ? 'contain' : 'not.contain', '_active_');
+});
