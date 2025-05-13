9681e5dc1dddafdf6c4aba07a01f6db5299b7fce
andres-codigo <52874360+andres-codigo@users.noreply.github.com>
2025-05-14 09:04:49 +1000

    [*.config.js] Update cypress.config.js
    - imported dotenv; updated env key from lowercase to uppercase
    - file formatted

--- cypress.config.js
+++ cypress.config.js
@@ -1,20 +1,24 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
+
+import dotenv from 'dotenv';
+dotenv.config();
 
 export default defineConfig({
-  e2e: {
-    baseUrl: "http://localhost:3000",
-    env: {
-      root_url: "/",
-      user_email: process.env.CYPRESS_USER_EMAIL,
-      user_password: process.env.CYPRESS_USER_PASSWORD,
-      vite_database_url: process.env.VITE_BACKEND_BASE_URL,
-    },
-  },
+	e2e: {
+		baseUrl: 'http://localhost:3000',
+		env: {
+			ROOT_URL: '/',
+			AUTHENTICATION_URL: '/authentication',
+			USER_EMAIL: process.env.CYPRESS_USER_EMAIL,
+			USER_PASSWORD: process.env.CYPRESS_USER_PASSWORD,
+			VITE_DATABASE_URL: process.env.VITE_BACKEND_BASE_URL,
+		},
+	},
 
-  component: {
-    devServer: {
-      framework: "react",
-      bundler: "vite",
-    },
-  },
+	component: {
+		devServer: {
+			framework: 'react',
+			bundler: 'vite',
+		},
+	},
 });
