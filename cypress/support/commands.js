937800c75dcec66d1084a4ef55baa81298e292b7
andres-codigo <52874360+andres-codigo@users.noreply.github.com>
2025-05-14 09:50:37 +1000

    [js] Update commands.js
    - login and setViewportToDesktop commands added

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,23 @@
-import { viewports, topNavigationSelectors } from './constants';
+import {
+	viewports,
+	topNavigationSelectors,
+	authenticationFormSelectors,
+} from './constants';
+
+// LOGIN
+Cypress.Commands.add('login', (email, password) => {
+	cy.get(authenticationFormSelectors.emailInput).type(email);
+	cy.get(authenticationFormSelectors.passwordInput).type(password);
+	cy.get(authenticationFormSelectors.submitButtonLogin).click();
+});
 
 // VIEWPORTS
+Cypress.Commands.add('setViewportToDesktop', () => {
+	cy.window().then((win) => {
+		cy.viewport(viewports.desktop.width, win.innerHeight);
+	});
+});
+
 Cypress.Commands.add('setViewportToMobile', () => {
 	cy.window().then((win) => {
 		cy.viewport(viewports.mobile.width, win.innerHeight);
