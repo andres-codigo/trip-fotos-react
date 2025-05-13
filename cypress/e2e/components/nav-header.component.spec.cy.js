ce5cde984b5fe360e84adbe06bf8e4ddf03337d0
andres-codigo <52874360+andres-codigo@users.noreply.github.com>
2025-05-14 09:09:45 +1000

    [*.component.spec.cy.js] Update nav-header.component.spec.cy.js
    - imported necessary constants
    - added Desktop > Hamburger Menu Test for the opening and closing of hamburger menu on click

--- cypress/e2e/components/nav-header.component.spec.cy.js
+++ cypress/e2e/components/nav-header.component.spec.cy.js
@@ -1,8 +1,13 @@
-import { baseUrl, topNavigationSelectors } from '../../support/constants';
+import {
+	urls,
+	user,
+	topNavigationSelectors,
+	authenticationFormSelectors,
+} from '../../support/constants';
 
 describe('Desktop > User Login Render Tests', () => {
 	beforeEach(() => {
-		cy.visit(baseUrl);
+		cy.visit(urls.cyAuth);
 	});
 
 	it('The top navigation container displays the app title as a link', () => {
@@ -16,5 +21,34 @@ describe('Desktop > User Login Render Tests', () => {
 			.then(($siteHeaderTitleLink) => {
 				expect($siteHeaderTitleLink.text()).to.equal('Trip Fotos');
 			});
+	});
+});
+
+describe('Desktop > Hamburger Menu Tests', () => {
+	beforeEach(() => {
+		cy.visit(urls.cyAuth);
+
+		cy.get(authenticationFormSelectors.emailInput).type(user.email);
+		cy.get(authenticationFormSelectors.passwordInput).type(user.password);
+		cy.get(authenticationFormSelectors.submitButtonLogin).click();
+	});
+	it('Opens and closes the hamburger menu when clicked', () => {
+		cy.setViewportToMobile();
+
+		cy.get(topNavigationSelectors.siteHeader).as('header');
+		cy.get('@header')
+			.find(topNavigationSelectors.navHamburgerMenu)
+			.as('hamburgerMenu');
+
+		// Ensure the menu is initially closed
+		cy.assertHamburgerMenuState(false);
+
+		// Click to open the menu
+		cy.get('@hamburgerMenu').click();
+		cy.assertHamburgerMenuState(true);
+
+		// Click to close the menu
+		cy.get('@hamburgerMenu').click();
+		cy.assertHamburgerMenuState(false);
 	});
 });
