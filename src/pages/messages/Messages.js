0f44090df96bcb1c2cba8fa7bc23b91de3d752ce
andres-codigo <52874360+andres-codigo@users.noreply.github.com>
2025-05-15 14:40:23 +1000

    [js, scss] Copilot suggested update update

--- src/pages/messages/Messages.js
+++ src/pages/messages/Messages.js
@@ -2,7 +2,7 @@ import MessagesStyles from './Messages.module.scss';
 
 const Messages = () => {
 	return (
-		<section className={MessagesStyles.tripsContainer}>
+		<section className={MessagesStyles.messagesContainer}>
 			<h1>Messages page</h1>
 		</section>
 	);
