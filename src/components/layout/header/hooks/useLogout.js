c8339b92f85ea9eaedbc7fe93d7715d8b557efac
andres-codigo <52874360+andres-codigo@users.noreply.github.com>
2025-05-20 15:00:25 +1000

    [jsx, js] Updated routing
    - Added a useEffect to watch for changes in 'isLoggedIn' and to always redirect to the login page if user is not logged in; removed isLoggedIn conditions around routes as not required, and removed navigation from useLogout hook
    - remove 'to' prop from NavMenuButtonLink logout component as not required

--- src/components/layout/header/hooks/useLogout.js
+++ src/components/layout/header/hooks/useLogout.js
@@ -1,13 +1,9 @@
 import { useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-
-import { PATHS } from '@/constants/paths'
 
 import { logout } from '@/store/slices/authenticationSlice'
 
 export function useLogout(setTravellerName, setTotalMessages, setIsMenuOpen) {
 	const dispatch = useDispatch()
-	const navigate = useNavigate()
 
 	const handleLogoutClick = (event) => {
 		event.preventDefault()
@@ -17,8 +13,6 @@ export function useLogout(setTravellerName, setTotalMessages, setIsMenuOpen) {
 		setIsMenuOpen(false)
 
 		dispatch(logout())
-
-		navigate(PATHS.AUTHENTICATION)
 	}
 
 	return handleLogoutClick
