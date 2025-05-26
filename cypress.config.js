a03ff76711df2e196adedfb12162456aa71bceaa
andres-codigo <52874360+andres-codigo@users.noreply.github.com>
2025-05-26 11:49:38 +1000

    Update cypress.config.js
    environment object moved as a top-level option; added PAGENOTFOUND and NON_EXISTENT_ROUTE variables

--- cypress.config.js
+++ cypress.config.js
@@ -6,20 +6,23 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export default defineConfig({
+	env: {
+		// PATHS
+		ROOT_URL: PATHS.HOME,
+		AUTHENTICATION_URL: PATHS.AUTHENTICATION,
+		TRIPS_URL: PATHS.TRIPS,
+		MESSAGES_URL: PATHS.MESSAGES,
+		PAGENOTFOUND: PATHS.PAGENOTFOUND,
+		// TEST PATHS
+		NON_EXISTENT_ROUTE: PATHS.NON_EXISTENT_ROUTE,
+		// API
+		USER_EMAIL: process.env.CYPRESS_USER_EMAIL,
+		USER_PASSWORD: process.env.CYPRESS_USER_PASSWORD,
+		// BACKEND
+		VITE_DATABASE_URL: process.env.VITE_BACKEND_BASE_URL,
+	},
 	e2e: {
-		baseUrl: 'http://localhost:3000',
-		env: {
-			// PATHS
-			ROOT_URL: PATHS.HOME,
-			AUTHENTICATION_URL: PATHS.AUTHENTICATION,
-			TRIPS_URL: PATHS.TRIPS,
-			MESSAGES_URL: PATHS.MESSAGES,
-			// API
-			USER_EMAIL: process.env.CYPRESS_USER_EMAIL,
-			USER_PASSWORD: process.env.CYPRESS_USER_PASSWORD,
-			// BACKEND
-			VITE_DATABASE_URL: process.env.VITE_BACKEND_BASE_URL,
-		},
+		baseUrl: process.env.VITE_ROOT_URL,
 	},
 
 	component: {
