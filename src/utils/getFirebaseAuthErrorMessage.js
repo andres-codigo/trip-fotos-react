36a9018704234a81e0cb3265838d462e6efc47ea
andres-codigo <52874360+andres-codigo@users.noreply.github.com>
2025-05-27 12:30:38 +1000

    Update getFirebaseAuthErrorMessage.js
    TOO_MANY_ATTEMPTS_TRY_LATER case added

--- src/utils/getFirebaseAuthErrorMessage.js
+++ src/utils/getFirebaseAuthErrorMessage.js
@@ -2,6 +2,8 @@ export const getFirebaseAuthErrorMessage = (error) => {
 	switch (error) {
 		case 'INVALID_LOGIN_CREDENTIALS':
 			return 'The email or password you entered is incorrect.'
+		case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+			return 'Too many unsuccessful login attempts. Please try again later.'
 		default:
 			return 'An unexpected error occurred. Please try again.'
 	}
