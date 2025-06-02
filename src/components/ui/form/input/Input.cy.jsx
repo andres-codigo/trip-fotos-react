90fcf3aceaff3e245d5356a5759bf0f86c3dc581
andres-codigo <52874360+andres-codigo@users.noreply.github.com>
2025-06-03 09:52:53 +1000

    Update Input.cy.jsx
    Renders as a disabled input field when disable prop is true

--- src/components/ui/form/input/Input.cy.jsx
+++ src/components/ui/form/input/Input.cy.jsx
@@ -32,6 +32,21 @@ describe('<Input />', () => {
 		cy.get(`[data-cy="${TEST_IDS.INPUT}"]`).should('have.value', 'No label')
 	})
 
+	it('renders as disabled when disabled prop is true', () => {
+		cy.mount(
+			<Input
+				id={TEST_IDS.INPUT}
+				label="Disabled Input"
+				value="Can't edit"
+				onChange={() => {}}
+				isValid={true}
+				dataCyInput={TEST_IDS.INPUT}
+				disabled={true}
+			/>,
+		)
+		cy.get(`[data-cy="${TEST_IDS.INPUT}"]`).should('be.disabled')
+	})
+
 	it('calls onChange when typing', () => {
 		const handleChange = cy.stub().as('onChange')
 		cy.mount(
