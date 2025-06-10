777c9004638f772d20021b721d5d06cfbaf7da21
andres-codigo <52874360+andres-codigo@users.noreply.github.com>
2025-06-10 12:21:13 +1000

    Updated Header component
    Header.jsx
    - Added removed h1 from MainNav component and converted into react Link
    
    Header.module.scss
    - updated justify-content css property  and added h1. siteHeaderTitleLink css properties

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,12 +1,27 @@
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+
+import { PATHS } from '@/constants/paths'
+
 import MainNav from '../main-nav/MainNav'
 
 import headerStyles from './Header.module.scss'
 
 function Header() {
+	const isLoggedIn = useSelector(
+		(state) => state.authentication.token !== null,
+	)
 	return (
 		<header
 			className={headerStyles.siteHeader}
 			data-cy="site-header">
+			<h1
+				className={headerStyles.siteHeaderTitleLink}
+				data-cy="site-header-title-link">
+				<Link to={isLoggedIn ? PATHS.TRIPS : PATHS.AUTHENTICATION}>
+					Trip Fotos
+				</Link>
+			</h1>
 			<MainNav />
 		</header>
 	)
