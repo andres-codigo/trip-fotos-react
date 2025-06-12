634fc94d11677159fb4594b6d0593109f2e30ab8
andres-codigo <52874360+andres-codigo@users.noreply.github.com>
2025-06-13 09:50:59 +1000

    Updated BrowserRouter
    BrowserRouter removed from index.js and moved into App.jsx to resolve Cypress component issue around testing of React Router url's

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import { Analytics } from '@vercel/analytics/react'
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from '@/store/store'
 
@@ -13,9 +12,7 @@ import '@/styles/global.scss'
 createRoot(document.getElementById('app')).render(
 	<StrictMode>
 		<Provider store={store}>
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
+			<App />
 			<Analytics />
 		</Provider>
 	</StrictMode>,
