60eea47ce19618192f9390d89c93eba197794214
andres-codigo <52874360+andres-codigo@users.noreply.github.com>
2025-06-13 15:04:16 +1000

    Updated Header Cypress test
    - moved helper functions into separate file
    - imported helper functions to Helper.cy.jsx

--- src/components/layout/header/__tests__/Header.cy.jsx
+++ src/components/layout/header/__tests__/Header.cy.jsx
@@ -1,65 +1,15 @@
 import { Provider } from 'react-redux'
 import { MemoryRouter } from 'react-router-dom'
 
-import {
-	urls,
-	headerSelectors,
-	topNavigationSelectors,
-} from '../../../../../cypress/support/constants'
+import { urls } from '../../../../../cypress/support/constants'
+
+import { headerAssertions } from './headerTestHelpers'
 
 import Header from '../Header'
 
 import configureStore from 'redux-mock-store'
 
 const mockStore = configureStore([])
-
-const assertHeaderTitleLink = (expectedHref) => {
-	cy.get(headerSelectors.siteHeaderTitleLink)
-		.should('exist')
-		.find('a')
-		.should('have.attr', 'href', expectedHref)
-		.and('contain.text', 'Trip Fotos')
-}
-
-const assertNavMenu = ({ shouldExist, shouldBeVisible }) => {
-	const navMenu = cy.get(topNavigationSelectors.navMenuContainer)
-	if (shouldExist) {
-		navMenu.should('exist')
-		const navMenuItems = cy.get(
-			topNavigationSelectors.navMenuItemsContainer,
-		)
-		if (shouldBeVisible) {
-			navMenuItems.should('be.visible')
-		} else {
-			navMenuItems.should('not.be.visible')
-		}
-	} else {
-		navMenu.should('not.exist')
-	}
-}
-
-const assertHamburgerMenu = (shouldExist) => {
-	const hamburgerMenu = cy.get(topNavigationSelectors.navHamburgerMenu)
-	if (shouldExist) {
-		hamburgerMenu.should('exist')
-	} else {
-		hamburgerMenu.should('not.exist')
-	}
-}
-
-const headerAssertions = (
-	expectedHref,
-	navMenuExists,
-	navMenuVisible,
-	hamburgerMenuExists,
-) => {
-	assertHeaderTitleLink(expectedHref)
-	assertNavMenu({
-		shouldExist: navMenuExists,
-		shouldBeVisible: navMenuVisible,
-	})
-	assertHamburgerMenu(hamburgerMenuExists)
-}
 
 describe('<Header />', () => {
 	describe('Rendering tests', () => {
---
+++ src/components/layout/header/__tests__/headerTestHelpers.js
@@ -0,0 +1,52 @@
+import {
+	headerSelectors,
+	topNavigationSelectors,
+} from '../../../../../cypress/support/constants'
+
+export const assertHeaderTitleLink = (expectedHref) => {
+	cy.get(headerSelectors.siteHeaderTitleLink)
+		.should('exist')
+		.find('a')
+		.should('have.attr', 'href', expectedHref)
+		.and('contain.text', 'Trip Fotos')
+}
+
+export const assertNavMenu = ({ shouldExist, shouldBeVisible }) => {
+	const navMenu = cy.get(topNavigationSelectors.navMenuContainer)
+	if (shouldExist) {
+		navMenu.should('exist')
+		const navMenuItems = cy.get(
+			topNavigationSelectors.navMenuItemsContainer,
+		)
+		if (shouldBeVisible) {
+			navMenuItems.should('be.visible')
+		} else {
+			navMenuItems.should('not.be.visible')
+		}
+	} else {
+		navMenu.should('not.exist')
+	}
+}
+
+export const assertHamburgerMenu = (shouldExist) => {
+	const hamburgerMenu = cy.get(topNavigationSelectors.navHamburgerMenu)
+	if (shouldExist) {
+		hamburgerMenu.should('exist')
+	} else {
+		hamburgerMenu.should('not.exist')
+	}
+}
+
+export const headerAssertions = (
+	expectedHref,
+	navMenuExists,
+	navMenuVisible,
+	hamburgerMenuExists,
+) => {
+	assertHeaderTitleLink(expectedHref)
+	assertNavMenu({
+		shouldExist: navMenuExists,
+		shouldBeVisible: navMenuVisible,
+	})
+	assertHamburgerMenu(hamburgerMenuExists)
+}
