b8e232007e0a59fa590fa798d7332c81439a79c3
andres-codigo <52874360+andres-codigo@users.noreply.github.com>
2025-06-13 15:40:15 +1000

    Update Header.cy.jsx
    - removed mock store and mount and replaced with newly created Cypress Helper commands
    - added accessibility tests

--- src/components/layout/header/__tests__/Header.cy.jsx
+++ src/components/layout/header/__tests__/Header.cy.jsx
@@ -1,66 +1,85 @@
-import { Provider } from 'react-redux'
-import { MemoryRouter } from 'react-router-dom'
-
-import { urls } from '../../../../../cypress/support/constants'
+import { urls, headerSelectors } from '../../../../../cypress/support/constants'
 
 import { headerAssertions } from './headerTestHelpers'
 
 import Header from '../Header'
 
-import configureStore from 'redux-mock-store'
-
-const mockStore = configureStore([])
-
 describe('<Header />', () => {
 	describe('Rendering tests', () => {
 		it('renders correctly for not logged in users on mobile, tablet, and desktop', () => {
-			const store = mockStore({
-				authentication: { token: null },
+			cy.createMockStore(null).then((store) => {
+				cy.mountWithProviders(<Header />, store)
+
+				cy.setViewportToMobile()
+				headerAssertions(urls.cyAuth, false, false, false)
+
+				cy.setViewportToTablet()
+				headerAssertions(urls.cyAuth, false, false, false)
+
+				cy.setViewportToDesktop()
+				headerAssertions(urls.cyAuth, false, false, false)
 			})
-
-			cy.mount(
-				<Provider store={store}>
-					<MemoryRouter>
-						<Header />
-					</MemoryRouter>
-				</Provider>,
-			)
-
-			cy.setViewportToMobile()
-			headerAssertions(urls.cyAuth, false, false, false)
-
-			cy.setViewportToTablet()
-			headerAssertions(urls.cyAuth, false, false, false)
-
-			cy.setViewportToDesktop()
-			headerAssertions(urls.cyAuth, false, false, false)
 		})
 
 		it('renders correctly for logged in users on mobile, tablet, and desktop', () => {
-			const store = mockStore({
-				authentication: { token: 'fake-token' },
+			cy.createMockStore('fake-token').then((store) => {
+				cy.mountWithProviders(<Header />, store)
+
+				cy.setViewportToMobile()
+				headerAssertions(urls.cyTrips, true, false, true)
+
+				cy.setViewportToTablet()
+				headerAssertions(urls.cyTrips, true, true, true)
+
+				cy.setViewportToDesktop()
+				headerAssertions(urls.cyTrips, true, true, true)
 			})
-
-			cy.mount(
-				<Provider store={store}>
-					<MemoryRouter>
-						<Header />
-					</MemoryRouter>
-				</Provider>,
-			)
-
-			cy.setViewportToMobile()
-			headerAssertions(urls.cyTrips, true, false, true)
-
-			cy.setViewportToTablet()
-			headerAssertions(urls.cyTrips, true, true, true)
-
-			cy.setViewportToDesktop()
-			headerAssertions(urls.cyTrips, true, true, true)
 		})
 	})
 
 	describe('Behaviour tests', () => {})
 
-	describe('Accessibility tests', () => {})
+	describe('Accessibility tests', () => {
+		describe('Not logged in', () => {
+			it('has accessibility labels on header', () => {
+				cy.createMockStore(null).then((store) => {
+					cy.mountWithProviders(<Header />, store)
+
+					cy.get(headerSelectors.siteHeader)
+						.should('have.attr', 'role', 'banner')
+						.and('have.attr', 'aria-label', 'Site header')
+
+					cy.get(`${headerSelectors.siteHeaderTitleLink} a`).should(
+						'have.attr',
+						'aria-label',
+						'Trip Fotos Home',
+					)
+				})
+			})
+		})
+
+		describe('Logged in', () => {
+			it('has accessibility labels on header and navigation', () => {
+				cy.createMockStore('fake-token').then((store) => {
+					cy.mountWithProviders(<Header />, store)
+
+					cy.get(headerSelectors.siteHeader)
+						.should('have.attr', 'role', 'banner')
+						.and('have.attr', 'aria-label', 'Site header')
+
+					cy.get(`${headerSelectors.siteHeaderTitleLink} a`).should(
+						'have.attr',
+						'aria-label',
+						'Trip Fotos Home',
+					)
+
+					cy.get('nav').should(
+						'have.attr',
+						'aria-label',
+						'Top navigation',
+					)
+				})
+			})
+		})
+	})
 })
