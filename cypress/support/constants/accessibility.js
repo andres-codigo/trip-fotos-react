b98043fe3597efaa1aab61b384e465af0887f04b
andres-codigo <52874360+andres-codigo@users.noreply.github.com>
2025-06-16 10:30:39 +1000

    Create accessibility.js
    accessibilitySelectors constant created

---
+++ cypress/support/constants/accessibility.js
@@ -0,0 +1,10 @@
+export const accessibilitySelectors = {
+	ARIA_LABELLEDBY: 'aria-labelledby',
+	ARIA_REQUIRED: 'aria-required',
+	ARIA_INVALID: 'aria-invalid',
+	ARIA_DESCRIBEDBY: 'aria-describedby',
+	ARIA_BUSY: 'aria-busy',
+	ROLE: 'role',
+	ALERT: 'alert',
+	REQUIRED: 'required',
+}
