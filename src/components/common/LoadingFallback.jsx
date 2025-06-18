282c7ee3f2859f3192ab2730931abb6d1079971f
andres-codigo <52874360+andres-codigo@users.noreply.github.com>
2025-06-18 12:52:43 +1000

    Create LoadingFallback.jsx

---
+++ src/components/common/LoadingFallback.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types'
+
+import BaseCard from '@/components/ui/card/BaseCard'
+import BaseSpinner from '@/components/ui/spinner/BaseSpinner'
+
+import './LoadingFallback.module.scss'
+
+const LoadingFallback = ({ isComponent }) => {
+	return isComponent ? (
+		<BaseCard className="loadingFallback">
+			<BaseSpinner />
+		</BaseCard>
+	) : (
+		<main className="mainContainer">
+			<BaseCard className="loadingFallback">
+				<BaseSpinner />
+			</BaseCard>
+		</main>
+	)
+}
+
+LoadingFallback.propTypes = {
+	isComponent: PropTypes.bool.isRequired,
+}
+
+export default LoadingFallback
