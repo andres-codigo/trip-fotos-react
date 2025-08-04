8deb4ffc13605790223e0ea68366ec2f8c1c1a3a
andres-codigo <52874360+andres-codigo@users.noreply.github.com>
2025-08-04 12:42:07 +1000

    Update index.js
    - Removed comments

--- src/components/layout/main-nav/hooks/useLoggedInTravellerName/index.js
+++ src/components/layout/main-nav/hooks/useLoggedInTravellerName/index.js
@@ -5,20 +5,16 @@ export function useLoggedInTravellerName() {
 		() => localStorage.getItem('userName') || '',
 	)
 
-	// Function to sync state with localStorage
 	const syncTravellerName = useCallback(() => {
 		const newValue = localStorage.getItem('userName')
 		setTravellerName(newValue || '')
 	}, [])
 
 	useEffect(() => {
-		// Sync state on mount
 		syncTravellerName()
 
-		// Add event listener for storage changes
 		window.addEventListener('storage', syncTravellerName)
 
-		// Cleanup event listener on unmount
 		return () => {
 			window.removeEventListener('storage', syncTravellerName)
 		}
