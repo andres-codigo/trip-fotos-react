6b3848a091e67ac999f889aff90b8ca544d26705
andres-codigo <52874360+andres-codigo@users.noreply.github.com>
2025-10-20 12:58:59 +1100

    Refactor TravellersList to use Redux state and styles
    Updated TravellersList to fetch travellers from Redux store, handle loading and error states, and use CSS modules for styling. Added refresh functionality and improved conditional rendering for empty and loading states.

--- src/components/travellers/TravellersList/TravellersList.jsx
+++ src/components/travellers/TravellersList/TravellersList.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import { useSelector } from 'react-redux'
+import { useState, useEffect, useCallback } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import PropTypes from 'prop-types'
 
 import { GLOBAL } from '@/constants/global'
@@ -7,27 +7,59 @@ import { PATHS } from '@/constants/paths'
 
 import { selectIsAuthenticated } from '@/store/slices/authenticationSlice'
 
-import BaseCard from '@/components/ui/card/BaseCard'
+import {
+	loadTravellers,
+	selectTravellers,
+	selectHasTravellers,
+	selectIsTraveller,
+} from '@/store/slices/travellersSlice'
 
+import BaseCard from '@/components/ui/card/BaseCard'
 import BaseDialog from '@/components/ui/dialog/BaseDialog'
 import BaseButton from '@/components/ui/button/BaseButton'
-
 import BaseSpinner from '@/components/ui/spinner/BaseSpinner'
+
+import travellersListStyles from './TravellersList.module.scss'
 
 const TravellersList = ({ initialError = false, isLoading = false }) => {
 	const [error, setError] = useState(initialError)
+	const [loading, setLoading] = useState(isLoading)
 
+	const dispatch = useDispatch()
 	const isLoggedIn = useSelector(selectIsAuthenticated)
 
-	const isTraveller = useSelector(
-		(state) => state.travellers?.isTraveller || false,
+	const isTraveller = useSelector(selectIsTraveller)
+	const hasTravellers = useSelector(selectHasTravellers)
+	const travellers = useSelector(selectTravellers)
+
+	const loadTravellersHandler = useCallback(
+		async (refresh = false) => {
+			try {
+				setLoading(true)
+				await dispatch(loadTravellers({ forceRefresh: refresh }))
+				setLoading(false)
+			} catch (error) {
+				setError(error.message || 'Something went wrong!')
+				setLoading(false)
+			}
+		},
+		[dispatch],
 	)
+
+	useEffect(() => {
+		if (
+			!travellers ||
+			(Array.isArray(travellers) && travellers.length === 0)
+		) {
+			loadTravellersHandler()
+		}
+	}, [travellers, loadTravellersHandler])
 
 	const handleError = () => setError(null)
 
 	return (
 		<section
-			className="pageSection travellerListContainer"
+			className={`pageSection ${travellersListStyles.travellerListContainer}`}
 			data-cy="travellers-list-container">
 			{error && (
 				<BaseDialog
@@ -41,18 +73,56 @@ const TravellersList = ({ initialError = false, isLoading = false }) => {
 			)}
 			<section>
 				<BaseCard>
-					<div className="controls">
+					<div
+						className={travellersListStyles.controls}
+						data-cy="controls">
+						<BaseButton
+							mode={
+								!hasTravellers && !isLoading
+									? // 	||
+										// (filteredTravellers &&
+										// 	filteredTravellers.length === 0)
+										'disabled'
+									: 'outline'
+							}
+							disabled={
+								!isLoading && !hasTravellers ? true : false
+							}
+							className={
+								!hasTravellers && !isLoading
+									? travellersListStyles.hide
+									: ''
+							}
+							onClick={() => loadTravellersHandler(true)}>
+							Refresh
+						</BaseButton>
 						{isLoggedIn && !isTraveller && !isLoading && (
 							<BaseButton
 								isLink
-								to={PATHS.REGISTER}>
+								to={PATHS.REGISTER}
+								data-cy="register-link">
 								Register as a Traveller
 							</BaseButton>
 						)}
 					</div>
-					{isLoading && (
-						<div className="spinnerContainer">
+					{loading && (
+						<div className={travellersListStyles.spinnerContainer}>
 							<BaseSpinner />
+						</div>
+					)}
+					{!loading && hasTravellers && (
+						<ul
+							className={travellersListStyles.travellersList}
+							data-cy="travellers-list">
+							<li data-cy="traveller-item">Trip Item</li>
+						</ul>
+					)}
+					{!isLoading && !hasTravellers && (
+						<div
+							className={
+								travellersListStyles.noTravellersContainer
+							}>
+							<h3>No travellers listed.</h3>
 						</div>
 					)}
 				</BaseCard>
