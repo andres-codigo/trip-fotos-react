cfd52ca864e06ccaef26d584398b643da7aea18e
andres-codigo <52874360+andres-codigo@users.noreply.github.com>
2025-12-08 16:42:48 +1100

    Refactor traveller registration form layout and styles
    Wrapped the TravellerRegistrationForm in a BaseCard and section, added a heading, and updated the SCSS to scope styles under a new .travellerRegistrationFormContainer class. This improves the form's structure, visual hierarchy, and maintainability.

--- src/components/forms/traveller-registration/TravellerRegistrationForm.jsx
+++ src/components/forms/traveller-registration/TravellerRegistrationForm.jsx
@@ -3,6 +3,8 @@ import styles from './TravellerRegistrationForm.module.scss'
 
 import { VALIDATION_MESSAGES } from '@/constants/validation/messages'
 
+import BaseCard from '@/components/ui/card/BaseCard'
+
 import Input from '@/components/ui/form/input/Input'
 import Textarea from '@/components/ui/form/textarea/Textarea'
 import Checkbox from '@/components/ui/form/checkbox/Checkbox'
@@ -97,102 +99,111 @@ const TravellerRegistrationForm = () => {
 	}
 
 	return (
-		<form
-			className={styles.form}
-			onSubmit={handleSubmit}
-			data-cy="traveller-registration-form">
-			<div
-				className={`${styles.formControl} ${!formData.firstName.isValid ? styles.invalid : ''}`}>
-				<Input
-					id="firstName"
-					label="First name"
-					value={formData.firstName.val}
-					onChange={handleInputChange}
-					isValid={formData.firstName.isValid}
-					message={VALIDATION_MESSAGES.FIRST_NAME_REQUIRED}
-					data-cy="input-first-name"
-				/>
-			</div>
-
-			<div
-				className={`${styles.formControl} ${!formData.lastName.isValid ? styles.invalid : ''}`}>
-				<Input
-					id="lastName"
-					label="Last name"
-					value={formData.lastName.val}
-					onChange={handleInputChange}
-					isValid={formData.lastName.isValid}
-					message={VALIDATION_MESSAGES.LAST_NAME_REQUIRED}
-					data-cy="input-last-name"
-				/>
-			</div>
-
-			<div
-				className={`${styles.formControl} ${!formData.description.isValid ? styles.invalid : ''}`}>
-				<Textarea
-					id="description"
-					label="Description"
-					rows={5}
-					value={formData.description.val}
-					onChange={handleInputChange}
-					isValid={formData.description.isValid}
-					message={VALIDATION_MESSAGES.DESCRIPTION_REQUIRED}
-					data-cy="input-description"
-				/>
-			</div>
-
-			<div
-				className={`${styles.formControl} ${!formData.days.isValid ? styles.invalid : ''}`}>
-				<Input
-					id="days"
-					label="Number of days spent in the city?"
-					type="number"
-					value={formData.days.val}
-					onChange={handleInputChange}
-					isValid={formData.days.isValid}
-					message={VALIDATION_MESSAGES.DAYS_REQUIRED}
-					data-cy="input-days"
-				/>
-			</div>
-
-			<div
-				className={`${styles.formControl} ${!formData.areas.isValid ? styles.invalid : ''}`}>
-				<h3 className={styles.checkboxLabel}>Cities visited</h3>
-				{areas.map((area) => (
+		<section
+			className={`pageSection ${styles.travellerRegistrationFormContainer}`}
+			data-cy="travellers-list-container">
+			<BaseCard>
+				<h1>Register as a Traveller</h1>
+				<form
+					className={styles.form}
+					onSubmit={handleSubmit}
+					data-cy="traveller-registration-form">
 					<div
-						key={area.id}
-						className={styles.checkboxGroup}>
-						<Checkbox
-							id={area.id}
-							label={area.label}
-							value={area.id}
-							checked={formData.areas.val.includes(area.id)}
-							onChange={handleCheckboxChange}
-							data-cy={`checkbox-${area.id}`}
+						className={`${styles.formControl} ${!formData.firstName.isValid ? styles.invalid : ''}`}>
+						<Input
+							id="firstName"
+							label="First name"
+							value={formData.firstName.val}
+							onChange={handleInputChange}
+							isValid={formData.firstName.isValid}
+							message={VALIDATION_MESSAGES.FIRST_NAME_REQUIRED}
+							data-cy="input-first-name"
 						/>
 					</div>
-				))}
-				{!formData.areas.isValid && (
-					<p>{VALIDATION_MESSAGES.CITIES_REQUIRED}</p>
-				)}
-			</div>
 
-			{/* TODO: AddFile component */}
+					<div
+						className={`${styles.formControl} ${!formData.lastName.isValid ? styles.invalid : ''}`}>
+						<Input
+							id="lastName"
+							label="Last name"
+							value={formData.lastName.val}
+							onChange={handleInputChange}
+							isValid={formData.lastName.isValid}
+							message={VALIDATION_MESSAGES.LAST_NAME_REQUIRED}
+							data-cy="input-last-name"
+						/>
+					</div>
 
-			{!formIsValid && (
-				<p className={styles.invalidForm}>
-					Please fix the above errors and submit again.
-				</p>
-			)}
+					<div
+						className={`${styles.formControl} ${!formData.description.isValid ? styles.invalid : ''}`}>
+						<Textarea
+							id="description"
+							label="Description"
+							rows={5}
+							value={formData.description.val}
+							onChange={handleInputChange}
+							isValid={formData.description.isValid}
+							message={VALIDATION_MESSAGES.DESCRIPTION_REQUIRED}
+							data-cy="input-description"
+						/>
+					</div>
 
-			<div className={styles.actions}>
-				<BaseButton
-					type="submit"
-					data-cy="submit-button">
-					Register
-				</BaseButton>
-			</div>
-		</form>
+					<div
+						className={`${styles.formControl} ${!formData.days.isValid ? styles.invalid : ''}`}>
+						<Input
+							id="days"
+							label="Number of days spent in the city?"
+							type="number"
+							value={formData.days.val}
+							onChange={handleInputChange}
+							isValid={formData.days.isValid}
+							message={VALIDATION_MESSAGES.DAYS_REQUIRED}
+							data-cy="input-days"
+						/>
+					</div>
+
+					<div
+						className={`${styles.formControl} ${!formData.areas.isValid ? styles.invalid : ''}`}>
+						<h3 className={styles.checkboxLabel}>Cities visited</h3>
+						{areas.map((area) => (
+							<div
+								key={area.id}
+								className={styles.checkboxGroup}>
+								<Checkbox
+									id={area.id}
+									label={area.label}
+									value={area.id}
+									checked={formData.areas.val.includes(
+										area.id,
+									)}
+									onChange={handleCheckboxChange}
+									data-cy={`checkbox-${area.id}`}
+								/>
+							</div>
+						))}
+						{!formData.areas.isValid && (
+							<p>{VALIDATION_MESSAGES.CITIES_REQUIRED}</p>
+						)}
+					</div>
+
+					{/* TODO: AddFile component */}
+
+					{!formIsValid && (
+						<p className={styles.invalidForm}>
+							Please fix the above errors and submit again.
+						</p>
+					)}
+
+					<div className={styles.actions}>
+						<BaseButton
+							type="submit"
+							data-cy="submit-button">
+							Register
+						</BaseButton>
+					</div>
+				</form>
+			</BaseCard>
+		</section>
 	)
 }
 
