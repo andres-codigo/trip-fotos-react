75dc9b2d2e9307eaa8af647e7ec537719cadeba8
Andres <52874360+andres-codigo@users.noreply.github.com>
2026-04-22 15:07:47 +1000

    test(cypress): add global browser state isolation (#170)

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,5 +16,11 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
+beforeEach(() => {
+	cy.clearAllCookies()
+	cy.clearAllLocalStorage()
+	cy.clearAllSessionStorage()
+})
+
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
